93b4227ef22cb68058f06ade58e87556badddd77
Rittick <rittick19971997@gmail.com>
2020-10-14 14:04:05 +0530

    update and delete course

--- controllers/courses.js
+++ controllers/courses.js
@@ -26,3 +26,65 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   let courses = await query;
   res.status(200).json({ success: true, count: courses.length, data: courses });
 });
+
+//routing                    /api/v1/courses/:id
+//method                     GET
+//access                     public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    next(new ErrorResponse(`course not found with the id ${req.params.id}`, 404));
+    return;
+  }
+  res.status(200).json({ success: true, data: course });
+});
+
+//routing                    /api/v1/bootcamps/:bootcampId/courses
+//method                     POST
+//access                     private
+exports.addCourses = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  if (!bootcamp) {
+    next(new ErrorResponse(`bootcamp not found with the id ${req.params.bootcampId}`, 400));
+    return;
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, data: course });
+});
+
+//routing                    /api/v1/courses/:id
+//method                     PUT
+//access                     private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    next(new ErrorResponse(`course not found with the id ${req.params.id}`, 404));
+    return;
+  }
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({ success: true, data: course });
+});
+
+//routing                    /api/v1/courses/:id
+//method                     DELETE
+//access                     private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    next(new ErrorResponse(`course not found with the id ${req.params.id}`, 404));
+    return;
+  }
+  course.remove();
+
+  res.status(200).json({ success: true, data: course });
+});
